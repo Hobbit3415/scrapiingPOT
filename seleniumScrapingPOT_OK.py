783f7bed8d9d6a050b8d571b640e5495b8ef4ebe
pcpablo529 <60487711+pcpablo529@users.noreply.github.com>
2022-11-09 14:50:38 -0500

    Add files via upload

--- seleniumScrapingPOT_OK.py
+++ seleniumScrapingPOT_OK.py
@@ -27,7 +27,8 @@ def normalise(s):
         ("%C3%A1", "á"),
         ("%C3%AD", "í"),
         ("%C3%B1", "ñ"),
-        ('%C3%A9','é')
+        ('%C3%A9','é'),
+        ('%C3%BA','ú')
     )
     for a, b in replacements:
         s = s.replace(a, b).replace(a.upper(), b.upper())
@@ -45,15 +46,14 @@ def driver_f(url, xPath):
     time.sleep(25)
     # Lista que almacena el contenido de la etiqueta UL
     # XPATH del UL que almacena todos los resultados
-    wait = WebDriverWait(driver, 30)
+    wait = WebDriverWait(driver, 40)
     # driver.implicitly_wait(30)
     try:
         lista = wait.until(EC.element_to_be_clickable((By.XPATH, xPath)))
         lista.click()
     except ElementClickInterceptedException:
         print("Trying to click on the button again")
-        driver_.execute_script("arguments[0].click()", lista)
-        
+        driver_.execute_script("arguments[0].click()", lista)        
     # lista = driver.find_element(By.XPATH, xPath)
     time.sleep(5)
     # Cierra el navegador
@@ -88,7 +88,7 @@ time.sleep(10)
 
 wait = WebDriverWait(driver_, 20)
 
-for i in range(0,40):
+for i in range(0,35):
     try:
         showmore_link = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="ember106"]/button[1]')))
         showmore_link.click()
@@ -109,25 +109,42 @@ listaUl = driver_.find_element(By.XPATH, xPath)
 # "search-result"
 listaLi = listaUl.find_elements(By.CLASS_NAME, "search-result")
 
-names = ['Titulo', 'Enlace']  #Nombres de las columnas
+# Lista que almacena los enlaces
+listaEnlace = []
+
+# Generacion de Dataframe 
+names = ['Titulo', 'WebElement']  #Nombres de las columnas
 
 df = pd.DataFrame()
 df = pd.DataFrame(columns=names)
 
 for i in listaLi:
+    print('#######################')
+    print((normalise(os.path.split(i.find_element(By.TAG_NAME, "a").get_attribute('href'))[-1].replace('-',' '))))
+    print (i)
     # Busque el tag "a" e imprima su atributo 'href' (link) buscado
     df2 = pd.DataFrame(columns=names)
-    print(i.find_element(By.TAG_NAME, "a").get_attribute('href'))    
-    #Extraccion de la cola del link / titulo
-    print(os.path.split(i.find_element(By.TAG_NAME, "a").get_attribute('href'))[-1].replace('-',' '))
-    # Almacenar cada cola/titulo
-    # listaTitles.append(normalise(os.path.split(i.find_element(By.TAG_NAME, "a").get_attribute('href'))[-1].replace('-',' '))) 
+    # print(i.find_element(By.TAG_NAME, "a").get_attribute('href'))    
+    # Almacenar cada titulo
     df2['Titulo'] = [(normalise(os.path.split(i.find_element(By.TAG_NAME, "a").get_attribute('href'))[-1].replace('-',' ')))]
-    # listaTitles.append(normalise(os.path.split(i.find_element(By.TAG_NAME, "a").get_attribute('href'))[-1].replace('-',' '))) 
-    listaDiv = driver_f(i.find_element(By.TAG_NAME, "a").get_attribute('href'),'//*[@id="main-region"]/div[1]/div[3]/div[2]')
-    # Almacenar cada link en una lista para realizar scraping a cada uno
-    df2['Enlace'] = [(listaDiv.find_element(By.TAG_NAME, "a").get_attribute('href'))]
-    # listaLinks.append(listaDiv.find_element(By.TAG_NAME, "a").get_attribute('href'))
+    df2['WebElement'] = i
     df = df.append(df2, ignore_index=True)
-       
- 
+    
+# Lista que almacena los valores unicos de los titulos
+Titulos_dup = df['Titulo'].duplicated()
+
+names2 = ['Titulo', 'Enlace']  #Nombres de las columnas
+
+df_ = pd.DataFrame()
+df_ = pd.DataFrame(columns=names2)
+
+for i in range (62, len(Titulos_dup)):    
+    if Titulos_dup[i] == False:
+        print(i)
+        print(Titulos_dup[i])
+        df3 = pd.DataFrame(columns=names2)
+        df3['Titulo'] = [df.iloc[i]['Titulo']]
+        listaDiv = driver_f((df.iloc[i]['WebElement']).find_element(By.TAG_NAME, "a").get_attribute('href'),'//*[@id="main-region"]/div[1]/div[3]/div[2]')
+        df3['Enlace'] = [(listaDiv.find_element(By.TAG_NAME, "a").get_attribute('href'))]
+        df_ = df_.append(df3, ignore_index=True)
+        
