7034cabf3cae36d8961b468caa1d243adc73a0c9
Hobbit3415 <emmanuel.mora3415@gmail.com>
2022-12-12 10:27:45 -0500

    Update

--- seleniumScrapingPOT_OK.py
+++ seleniumScrapingPOT_OK.py
@@ -6,21 +6,6 @@ Created on Wed Nov  2 18:44:40 2022
 @author: hobbit3415
 """
 
-<<<<<<< HEAD
-# In[01]
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import ElementClickInterceptedException
-from selenium.webdriver.chrome.options import Options
-import time
-import os
-import pandas as pd 
-# import wget
-=======
 # In[]
 import os
 import time
@@ -45,7 +30,6 @@ from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
 import re
 from lxml import etree
->>>>>>> testing
 
 # Funciones
 os.environ['GH_TOKEN'] = "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"
@@ -67,13 +51,8 @@ def normalise(s):
         ("%C3%A1", "á"),
         ("%C3%AD", "í"),
         ("%C3%B1", "ñ"),
-<<<<<<< HEAD
-        ('%C3%A9','é'),
-        ('%C3%BA','ú')
-=======
         ('%C3%A9', 'é'),
         ('%C3%BA', 'ú')
->>>>>>> testing
     )
     for a, b in replacements:
         s = s.replace(a, b).replace(a.upper(), b.upper())
@@ -110,13 +89,6 @@ def open_driver_firefox(url):
     driver.get(url)
     # Duerme el hilo por 10 segundos para asegurarse de que todo el
     # contenido dinamico se cargue completamente
-<<<<<<< HEAD
-    time.sleep(25)
-    # Lista que almacena el contenido de la etiqueta UL
-    # XPATH del UL que almacena todos los resultados
-    wait = WebDriverWait(driver, 40)
-    # driver.implicitly_wait(30)
-=======
     time.sleep(6)
 
     return driver
@@ -173,24 +145,16 @@ def driver_f(url, xPath):
     # Lista que almacena el contenido de la etiqueta UL
     # XPATH del UL que almacena todos los resultados
     wait = WebDriverWait(driver, 6)
->>>>>>> testing
     try:
         lista = wait.until(EC.element_to_be_clickable((By.XPATH, xPath)))
         lista.click()
     except ElementClickInterceptedException:
         print("Trying to click on the button again")
-<<<<<<< HEAD
-        driver_.execute_script("arguments[0].click()", lista)        
-    # lista = driver.find_element(By.XPATH, xPath)
-    time.sleep(5)
-    # Cierra el navegador
-    # driver.quit()
-=======
         driver_.execute_script("arguments[0].click()", lista)
 
     time.sleep(5)
->>>>>>> testing
     return lista
+
 
 url = "https://mapasyestadisticas-cundinamarca-map.opendata.arcgis.com/search?collection=Dataset&type=file%20geodatabase"
 
@@ -200,32 +164,6 @@ def check_exists_by_xpath(driver, xpath):
     Metodo que verifica que un elemento existe en el sitio
     web que se está consultando
 
-<<<<<<< HEAD
-options = Options()
-options.add_argument("--disable-notifications")
-options.add_argument("enable-automation")
-options.add_argument("--headless")
-options.add_argument("--window-size=1920,1080")
-options.add_argument("--no-sandbox")
-options.add_argument("--disable-extensions")
-options.add_argument("--dns-prefetch-disable")
-options.add_argument("--disable-gpu")
-
-# Instancia de un webdriver con el servicio de Chrome
-driver_ = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-
-# Abre un navegador de GoogleChrome y carga la URL solicitada
-# Carga por completo todos los scripts del sitio web y descarga
-# el HTML conn el contenido dinamico
-driver_.get(url)
-# Duerme el hilo por 10 segundos para asegurarse de que todo el
-# contenido dinamico se cargue completamente
-time.sleep(10)
-
-wait = WebDriverWait(driver_, 20)
-
-for i in range(0,35):
-=======
     Params:
         driver: Elemento de tipo web driver en donde se realizará
         la busqueda del elemento
@@ -237,7 +175,6 @@ for i in range(0,35):
         True si el elemento existe
         False si el elemento no existe
     """
->>>>>>> testing
     try:
         # driver.implicitly_wait(6)
         driver.find_element(By.XPATH, xpath)
@@ -396,12 +333,6 @@ for i in range(0, 19):
         driver_.execute_script("arguments[0].click()", showmore_link)
     except EC.StaleElementReferenceException:
         print('Botón no encontrado')
-<<<<<<< HEAD
-
-# XPATH del UL que almacena todos los resultados
-xPath = '//*[@id="search-results"]'
-
-=======
 
 
 # In[]
@@ -410,7 +341,6 @@ xPath = '//*[@id="search-results"]'
 # XPATH del UL que almacena todos los resultados
 xPath = '//*[@id="search-results"]'
 
->>>>>>> testing
 # Lista que almacena el contenido de la etiqueta UL
 listaUl = driver_.find_element(By.XPATH, xPath)
 
@@ -422,49 +352,12 @@ listaLi = listaUl.find_elements(By.CLASS_NAME, "search-result")
 # Lista que almacena los enlaces
 listaEnlace = []
 
-<<<<<<< HEAD
-# Generacion de Dataframe 
-names = ['Titulo', 'WebElement']  #Nombres de las columnas
-=======
 # Generacion de Dataframe
 names = ['Titulo', 'Enlace_web', 'Enlace_recursos', 'Etiquetas']
->>>>>>> testing
 
 df = pd.DataFrame()
 df = pd.DataFrame(columns=names)
 
-<<<<<<< HEAD
-for i in listaLi:
-    print('#######################')
-    print((normalise(os.path.split(i.find_element(By.TAG_NAME, "a").get_attribute('href'))[-1].replace('-',' '))))
-    print (i)
-    # Busque el tag "a" e imprima su atributo 'href' (link) buscado
-    df2 = pd.DataFrame(columns=names)
-    # print(i.find_element(By.TAG_NAME, "a").get_attribute('href'))    
-    # Almacenar cada titulo
-    df2['Titulo'] = [(normalise(os.path.split(i.find_element(By.TAG_NAME, "a").get_attribute('href'))[-1].replace('-',' ')))]
-    df2['WebElement'] = i
-    df = df.append(df2, ignore_index=True)
-    
-# Lista que almacena los valores unicos de los titulos
-Titulos_dup = df['Titulo'].duplicated()
-
-names2 = ['Titulo', 'Enlace']  #Nombres de las columnas
-
-df_ = pd.DataFrame()
-df_ = pd.DataFrame(columns=names2)
-
-for i in range (62, len(Titulos_dup)):    
-    if Titulos_dup[i] == False:
-        print(i)
-        print(Titulos_dup[i])
-        df3 = pd.DataFrame(columns=names2)
-        df3['Titulo'] = [df.iloc[i]['Titulo']]
-        listaDiv = driver_f((df.iloc[i]['WebElement']).find_element(By.TAG_NAME, "a").get_attribute('href'),'//*[@id="main-region"]/div[1]/div[3]/div[2]')
-        df3['Enlace'] = [(listaDiv.find_element(By.TAG_NAME, "a").get_attribute('href'))]
-        df_ = df_.append(df3, ignore_index=True)
-        
-=======
 # In[]
 # Obteniendo URL de cada sitio web que contiene el recurso buscado
 for i in listaLi:
@@ -530,4 +423,3 @@ df.to_clipboard()
 
 
 # %%
->>>>>>> testing
